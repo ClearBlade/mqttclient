459e6f887ee107a746bd50f0985ed2552d4e8e0e
Patrick Crawford <pcrawford@clearblade.com>
2014-12-12 13:03:28 -0600

    fixed timeouts

--- client.go
+++ client.go
@@ -153,15 +153,15 @@ func (c *Client) connectionListener() {
 			}
 			m <- msg
 		}
-
 	}(mch, ech)
-
+	sentPing := false
 	for {
 		select {
 
 		case msg := <-mch:
 			//dispatch the internal functions
 			c.dispatch(msg)
+			sentPing = false
 		case e := <-ech:
 			//an error was recieved from the listenr
 			if !shutdown {
@@ -172,11 +172,13 @@ func (c *Client) connectionListener() {
 			}
 		case <-time.After(c.Timeout):
 			//keepalive
-			if time.Since(c.last_timeout_reccd) > c.Timeout {
+			if !sentPing {
+				c.sendMessage(&mqtt.Pingreq{})
+				sentPing = true
+			} else if time.Since(c.last_timeout_reccd) > c.Timeout && sentPing {
 				//there was a timeout
 				c.Shutdown(true)
 			}
-			c.sendMessage(&mqtt.Pingreq{})
 
 		case <-c.shutdown_reader:
 			shutdown = true
